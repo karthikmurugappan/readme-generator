ef50665eab2f4ec09782d4259e7bbec38ddf0ae2
karthikmurugappan <karthikmurugappan7@gmail.com>
2023-06-18 14:46:32 -0400

    added all questions

--- index.js
+++ index.js
@@ -7,8 +7,85 @@ const questions = [
     {
         type: 'input',
         name: 'title',
-        message: 'What is the title of your project?',
-    }
+        message: 'Please provide the title for the project',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please provide a brief description',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Please provide installation notes',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Please provide instructions to effectively use project',
+    },
+    {
+        type: 'input',
+        name: 'license',
+        message: "Please select a license",
+        choices: [
+        {
+            name: 'MIT',
+            value: {
+                name: 'MIT',
+                badgeID: 'MIT',
+                link: 'https://opensource.org/licenses/MIT',
+            },
+        },
+        {
+            name: 'Apache',
+            value: {
+                name: 'Apache',
+                badgeID: 'Apache_2.0',
+                link: 'https://opensource.org/licenses/MIT',
+            },
+        },
+        {
+            name: 'GNU',
+            value: {
+                name: 'GNU',
+                badgeID: 'GPLv3',
+                link: 'https://www.gnu.org/licenses/gpl-3.0',
+            },
+        },
+        {
+            name: 'none',
+            value: {
+                name: 'none',
+                badgeID: 'none',
+                link: 'none',
+            },
+        },
+        ],
+    },
+    {
+        type: 'list',
+        name: 'collaborators',
+        message: "Did you work with any collaborators",
+        choices: ['Yes', 'No'],
+    },
+    {
+        type: 'input',
+        name: 'collabNames',
+        message: 'Please list the names of the collaborators',
+        when: (response) => response.collaborators === 'Yes',
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: "Please provide your GitHub username",
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Please provide your Email',
+    },
+
 ];
 
 // TODO: Create a function to write README file
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -13,23 +13,28 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-  
-  ## Description
 
-  ## Table of Contents
-  
-  ## Installation
-  
-  ## Usage
-  
-  ## License
-  
-  ## How to Contribute
-  
-  ## Tests
-  
-  ## Contact
-  `
+## Description
+${data.description}
+
+## Table of Contents
+
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## License
+${data.license}
+
+## How to Contribute
+
+## Tests
+
+## Contact
+`
 }
 
 module.exports = {generateMarkdown};
